d43be84e5f58df5b5096137d295d574d5937ca18
unknown <wahyusuteja688@gmail.com>
2023-12-07 13:45:19 +0700

    belajar node module system

---
+++ coba-dua.js
@@ -0,0 +1,7 @@
+// const cetakNama = require('./coba');
+// const PI = require('./coba');
+
+const coba = require('./coba')
+// console.log(coba);
+
+console.log(coba.cetakNama('Sandhika'), coba.PI, coba.mahasiswa.cetakMhs(), new coba.Orang);
